c952db07ad61b9ffb2f02c2dd0fbbec3e5a3760c
geniuslat <traber73@yandex.ru>
2025-01-17 23:20:07 +0500

    Deleted extra logging

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import telebot
 import random
 file=open("token.txt",'r')
 token=file.read().strip()#''
-
-for letter in token:
-  print(f" letter {letter} :{ord(letter)}")
-
 file.close()
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
